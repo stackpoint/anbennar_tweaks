6fbaacc8cb819686053fde89d3667e689d3efb39
afowl <ajnstajner@gmail.com>
2020-11-29 00:15:53 +0100

    defines from excelbel and dev click :D

---
+++ common/defines/NDefines.NCountry.lua
@@ -0,0 +1,42 @@
+-- Only Changes to NDefines.NCountry should be here.
+NDefines.NCountry.MERCHANT_REPUBLIC_SIZE_LIMIT = 9999
+NDefines.NCountry.MAX_ARMY_PROFESSIONALISM = 1.0
+NDefines.NCountry.LOW_ARMY_PROFESSIONALISM_MIN_RANGE = 0.0
+NDefines.NCountry.LOW_ARMY_PROFESSIONALISM_MAX_RANGE = 1.0
+NDefines.NCountry.HIGH_ARMY_PROFESSIONALISM_MIN_RANGE = 0.0
+NDefines.NCountry.HIGH_ARMY_PROFESSIONALISM_MAX_RANGE = 1.0
+NDefines.NCountry.STATE_MAINTENANCE_DEV_FACTOR = 0.012 --0.007 vanilla
+NDefines.NCountry.STATE_MAINTENANCE_DISTANCE_FACTOR = 0.002 --0.001 vanilla
+NDefines.NCountry.STATE_MAINTENANCE_CONTINENT_FACTOR = -0.5 --0.25 vanilla
+NDefines.NCountry.STATE_MAINTENANCE_CULTURE_FACTOR = 0.15 --0.25 vanilla
+NDefines.NCountry.SCRIPTED_ADVISOR_DISCOUNT = 0.33
+NDefines.NCountry.PS_BUY_IDEA = 300
+NDefines.NCountry.FREE_IDEA_GROUP_COST = 5
+NDefines.NCountry.IDEA_TO_TECH = -0.01
+NDefines.NCountry.MAX_ACTIVE_POLICIES = 12
+NDefines.NCountry.BASE_POSSIBLE_POLICIES = 4
+NDefines.NCountry.MINIMUM_POLICY_TIME = 3
+NDefines.NCountry.PS_RAISE_TARIFFS = 10
+NDefines.NCountry.PS_LOWER_TARIFFS = 5
+NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35
+NDefines.NCountry.DICTATORSHIP_TRADITION_FOR_MONARCHY = 20
+NDefines.NCountry.DICTATORSHIP_TRADITION_FOR_REPUBLIC = 60
+NDefines.NCountry.ESTATE_MIN_DISTRIBUTED_CROWNLAND = 33.0
+NDefines.NCountry.ESTATE_PRIVILEGES_MAX_CONCURRENT = 20
+NDefines.NCountry.ESTATE_AGENDA_ABOUT_TO_EXPIRE_WARNING = 425
+NDefines.NCountry.ESTATE_LOYALTY_DECAY_BASE_MIN = 6.0
+NDefines.NCountry.ESTATE_LOYALTY_DECAY_BASE_MAX = 9.0
+NDefines.NCountry.MONTHS_TO_CHANGE_CULTURE = 3
+NDefines.NCountry.SIBERIAN_FRONTIER_DAILY_BASE = 1		-- monthly..	anbennar ver this and bottom
+NDefines.NCountry.SIBERIAN_FRONTIER_DAILY_RANGE = 3
+
+NDefines.NCountry.CIRCUMNAVIGATION_PROVINCE_1 = 1502		-- Provinces for circumnavigation (coast of gambia)
+NDefines.NCountry.CIRCUMNAVIGATION_PROVINCE_2 = 1684		-- Provinces for circumnavigation (magellan strait)
+NDefines.NCountry.CIRCUMNAVIGATION_PROVINCE_3 = 1358		-- Provinces for circumnavigation (hawaii sea)
+NDefines.NCountry.CIRCUMNAVIGATION_PROVINCE_4 = 1446		-- Provinces for circumnavigation (hawaii sea)
+NDefines.NCountry.CIRCUMNAVIGATION_PROVINCE_5 = 1460		-- Provinces for circumnavigation (hawaii sea)
+NDefines.NCountry.CIRCUMNAVIGATION_PROVINCE_6 = 1467
+
+NDefines.NCountry.PIETY_PERCENTAGE_AT_NEW_RULER = 0.0			-- percentage of piety kept at new ruler.
+NDefines.NCountry.PIETY_INCREASE_AT_GOOD_WAR = -0.10
+NDefines.NCountry.PIETY_DECREASE_AT_BAD_WAR = -0.10
